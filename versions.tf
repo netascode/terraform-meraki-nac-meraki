68862c853cb0e98b07d9304b9cd4fe55d036bd11
Jon <jonhumph@cisco.com>
2024-08-21 08:54:49 +0100

    chore: Bump Meraki provider version to 0.2.11-alpha in versions.tf

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     meraki = {
       source  = "cisco-open/meraki"
-      version = "0.2.10-alpha"
+      version = "0.2.11-alpha"
     }
     utils = {
       source  = "netascode/utils"
