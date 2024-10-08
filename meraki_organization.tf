6c4abb3c6173418553692d6475cc8672f5725603
Jon <jonhumph@cisco.com>
2024-10-08 15:13:21 +0100

    Refactor meraki_organization.tf: Update adaptive policy dependency

--- meraki_organization.tf
+++ meraki_organization.tf
@@ -280,7 +280,10 @@ resource "meraki_organization_adaptive_policy" "adaptive_policies" {
   ]
 
   # last_entry_rule = "allow"
-  depends_on = [meraki_organization_adaptive_policy_acl.acls]
+  depends_on = [
+    meraki_organization_adaptive_policy_group.group,
+    meraki_organization_adaptive_policy_acl.acls
+  ]
 
 }
 
