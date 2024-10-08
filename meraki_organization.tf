4058908c87d0e32a95ad1429c84a28c224acffe9
Jon <jonhumph@cisco.com>
2024-10-08 19:08:50 +0100

    Refactor meraki_organization.tf: Update network ID retrieval for admins and adaptive policy resource names, dependencies, and variable names for source and destination ports, and improve network name lookup

--- meraki_organization.tf
+++ meraki_organization.tf
@@ -143,10 +143,10 @@ locals {
   ])
 }
 data "meraki_network" "networks" {
-  for_each = { for admin in local.admins : admin.key => admin }
+  for_each = { for admin in local.admins : admin.key => flatten([for network in try(admin.networks, []) : network.id]) }
 
   organization_id = each.value.organization_id
-  name            = each.value.networks[0] # Use the network name from the YAML for lookup (assuming one name for now)
+  name            = each.value
 }
 resource "meraki_organization_admin" "organization_admin_no_networks" {
   for_each              = { for admin in local.admins : admin.key => admin }
@@ -165,12 +165,15 @@ resource "meraki_organization_admin" "organization_admin_with_networks" {
   authentication_method = each.value.authentication_method
   org_access            = each.value.org_access
   tags                  = each.value.tags
+
+  # Use the dynamically fetched network IDs
   networks = [
     for network in try(each.value.networks, []) : {
-      id     = data.meraki_network.networks[network.id].id # Resolve the network ID
+      id     = data.meraki_network.networks[network.id].id # Fetch the network ID dynamically
       access = try(network.access, local.defaults.meraki.organizations.admins.networks.access, null)
     }
   ]
+
   depends_on = [meraki_network.network]
 }
 # Apply Organization Inventory Claim
