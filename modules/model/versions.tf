c5b4066dfc65b0bb1cbb0b0613ad239e98df3aec
Jon Humphries <jonhumph@cisco.com>
2026-06-01 13:58:06 +0100

    Update versions.tf (#174)
    * Update versions.tf
    
    Update to latest netascode utils tag and hashicorp/local.
    
    * Update versions.tf
    
    Changing flexibility to support minor patch releases.
    
    * Updating Readme.md

--- modules/model/versions.tf
+++ modules/model/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     utils = {
       source  = "netascode/utils"
-      version = ">= 0.2.6"
+      version = "~> 2.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = ">= 2.5.2"
+      version = "~> 2.9"
     }
   }
 }
